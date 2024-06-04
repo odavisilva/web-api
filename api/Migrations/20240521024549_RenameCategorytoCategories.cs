c887ebde644e21217426a50787f7d717a95c3bec
o.name <daviolie2@gmail.com>
2024-06-04 14:41:41 -0300

    Adicionando Produto

---
+++ api/Migrations/20240521024549_RenameCategorytoCategories.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCategorytoCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Category_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tag_Products_ProductId",
+                table: "Tag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "Tag",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tag_Products_ProductId",
+                table: "Tag",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tag_Products_ProductId",
+                table: "Tag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "Tag",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Category_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tag_Products_ProductId",
+                table: "Tag",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,21 +1,25 @@
-
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<Product.ApplicationDbContext>();
+builder.Services.AddSqlServer<Product.ApplicationDbContext>(builder.Configuration["Database:SqlServer"]);
 
 var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
  
-
- app.MapPost("/products", (Product product) => {
-    ProductRepository.Add(product);
-    return Results.Created($"/products/{product.Code}", product.Code);
+ app.MapPost("/products", (ProductDTO productDTO, Product.ApplicationDbContext context) => {
+    var category = context.Categories.Where(c => c.Id == productDTO.categoryId).First();
+    var product = new Product {
+        Code = productDTO.Code,
+        Name = productDTO.Name,
+        Description = productDTO.Description,
+        Category = category
+    };
+    context.Products.Add(product);
+    context.SaveChanges();
+    return Results.Created($"/products/{product.Id}", product.Id);
  });
-
 
 app.MapGet("products/{code}", ( [FromRoute] string code) => { 
     var product = ProductRepository.GetBy(code);
@@ -42,74 +46,4 @@ app.MapGet("/configuration/database", (IConfiguration configuration) => {
     return Results.Ok($"{configuration["Database:connection"]}/{configuration["Database:port"]}");
 });
 
-
 app.Run();
-
-public static class ProductRepository {
-    public static List<Product> Products { get; set; } = Products = new List<Product>();
-
-    public static void Init(IConfiguration configuration) {
-        var products = configuration.GetSection("Products").Get<List<Product>>();
-        Products = products;
-    }
-    public static void Add(Product product) {
-        Products.Add(product);
-    }
-
-    public static Product GetBy(string code) {
-        return Products.FirstOrDefault(p => p.Code == code);
-    }
-
-    public static void Remove(Product product) {
-        Products.Remove(product);
-    }
-}
-
-
-public class Category {
-
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
-
-public class Tag {
-    public int Id { get; set; }
-
-    public string Name { get; set; }
-
-    public int ProductId { get; set; }
-}
-public class Product {
-
-    public  int Id { get; set; }
-    public string Code { get; set; }
-
-    public string Name { get; set; }
-
-    public string Description { get; set; }
-
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
-    public List<Tag> Tags { get; set; }
-
-
-public class ApplicationDbContext : DbContext {
-    
-    public  DbSet<Product> Products { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.Entity<Product>()
-            .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
-        builder.Entity<Product>()
-            .Property(p => p.Name).HasMaxLength(120).IsRequired(false);
-        builder.Entity<Product>()
-            .Property(p => p.Code).HasMaxLength(20).IsRequired(false);
-    }
-    
-            protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer(
-                "Server=localhost;Database=Products;User Id=sa;Password=<YourStrong@Sql2022>;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
-
-    }
-}
